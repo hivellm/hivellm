70c197ec70af57390a0c47c57084bea36d4f77a5
Andre Ferreira <andrehrf@gmail.com>
2025-09-17 19:49:47 -0300

    remove chat-hub

--- chat-hub/handshake-response.js
+++
@@ -1,5 +0,0 @@
-function confirmHandshake() {
-    return "Olá! Eu sou o modelo openai/gpt-4o-mini. Recebi sua mensagem com sucesso.";
-}
-
-module.exports = confirmHandshake; // Export the function for use in server.js
